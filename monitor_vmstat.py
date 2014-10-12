ab4a3ec8b6a359311c652d9eece0e55e5688ea47
Arun Raman <arunraman@users.noreply.github.com>
2014-10-11 23:52:46 -0700

    Create monitor_vmstat.py
    usage: 
    vmstat | <script_name> <column_number> <repeat> <threshold>

---
+++ monitor_vmstat.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+__author__ = 'arun raman'
+
+import sys
+
+def read_vmst(column_num,repeat,threshold):
+    for linenum,line in enumerate(sys.stdin):
+        if linenum == 2:
+            l = line.split()
+            if repeat != 0:
+                if l[column_num - 1] > threshold :
+                    repeat -= 1
+                    print "Threshold reached!!"
+                    break
+
+def Main():
+    column_number = int(sys.argv[1])
+    repeat = int(sys.argv[2])
+    threshold = int(sys.argv[3])
+    read_vmst(column_number,repeat,threshold)
+
+if __name__ == '__main__':
+    Main()
